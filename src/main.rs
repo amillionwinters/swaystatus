4434052b9db8dff0ffd9947cad7e5ee766d0884c
alan baumann <amillionwinters@protonmail.com>
2025-01-20 04:12:51 -0600

    now using a buffer for to store the battery status and battery level reads to avoid making new allocations for each every iteration of the loop

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,46 @@
 use chrono::Local;
-use std::thread;
+use std::fs::File;
+use std::io::Read;
+use std::io::{Error, ErrorKind};
+use std::thread::sleep;
 use std::time::Duration;
 
-fn read_file_trimmed(path: &str) -> Result<String, std::io::Error> {
-    std::fs::read_to_string(path).map(|v| v.trim().to_string())
+fn read_file_trimmed<'a>(path: &str, buffer: &'a mut [u8]) -> Result<&'a str, Error> {
+    let mut file = File::open(path)?;
+    let bytes = file.read(buffer)?;
+    let content =
+        std::str::from_utf8(&buffer[..bytes]).map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+
+    Ok(content.trim())
 }
 
 fn main() {
     const CAPACITY: &str = "/sys/class/power_supply/BAT0/capacity";
     const CHARGE_STATUS: &str = "/sys/class/power_supply/BAT0/status";
+    const SLEEP_DURATION: Duration = Duration::new(1, 0);
+
+    let mut level_buf = [0u8; 16];
+    let mut status_buf = [0u8; 16];
 
     loop {
-        let battery_level = match read_file_trimmed(CAPACITY) {
+        let battery_level = match read_file_trimmed(CAPACITY, &mut level_buf) {
             Ok(v) => v,
-            Err(_) => "(Error Reading Battery Level)".to_owned(),
+            Err(_) => "(Error Reading Battery Level)",
         };
 
-        let now = Local::now();
-        let formatted_date = now.format("%A, %d %B");
-        let formatted_time = now.format("%H:%M:%S");
-
-        let battery_icon = match read_file_trimmed(CHARGE_STATUS) {
-            Ok(trimmed_status) => match trimmed_status.as_str() {
+        let battery_icon = match read_file_trimmed(CHARGE_STATUS, &mut status_buf) {
+            Ok(trimmed_status) => match trimmed_status {
                 "Discharging" => "󰁾",
                 _ => "󰂅",
             },
             Err(_) => "(Charging Status Unknown)",
         };
 
-        println!(
-            "{} {} // {} // {} ",
-            battery_icon, battery_level, formatted_date, formatted_time
-        );
+        let now = Local::now();
+        let formatted_date = now.format("%A, %d %B");
+        let formatted_time = now.format("%H:%M:%S");
 
-        thread::sleep(Duration::from_secs(1));
+        println!("{battery_icon} {battery_level}% // {formatted_date} // {formatted_time}");
+        sleep(SLEEP_DURATION);
     }
 }
